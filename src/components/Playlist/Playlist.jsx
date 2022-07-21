b49324b54576bd99512cebd7387a58813a6bebba
Patma12 <107095346+Patma12@users.noreply.github.com>
2022-07-21 21:24:30 +0700

    55

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 //import Playlist.css
+import './Playlist.css'
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends Component {
   render() {
-    return <div>Playlist</div>;
+    return (
+      <div className="Playlist">
+      <input value="New Playlist"/>
+      <TrackList  tracks={this.props.playlistTracks} 
+                  onRemove={this.props.onRemove}
+                  isRemoval={true}/>
+      <button className="Playlist-save">SAVE TO SPOTIFY</button>
+    </div>)
   }
 }
 
